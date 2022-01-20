d8f46d1828289d17e845674041533a998f4b6d7b
Nicky Sandhu <psandhu@water.ca.gov>
2022-01-19 19:48:14 -0800

    fixed iloc with boolean indexing

--- data_flag_editor/flag_checker.py
+++ data_flag_editor/flag_checker.py
@@ -1,12 +1,13 @@
+import panel as pn
+import param
 import pandas as pd
 import numpy as np
 import holoviews as hv
 from holoviews import streams
 from holoviews import opts, dim
 hv.extension('bokeh')
-import param
-import panel as pn
 pn.extension()
+
 
 class FlagChecker(param.Parameterized):
     flag = param.ObjectSelector(default="UNCHECKED", objects=[
@@ -14,18 +15,18 @@ class FlagChecker(param.Parameterized):
     undo_level = param.Integer(0, precedence=-1)
 
     def __init__(self, df, df_ref, **kwargs):
-        super().__init__(**kwargs) # param.Parameterized requires calling their super first
+        super().__init__(**kwargs)  # param.Parameterized requires calling their super first
         self.init(df, df_ref)
 
-    def init(self,df, df_ref):
+    def init(self, df, df_ref):
         self.x_col_name = 'x'
         self.y_col_name = 'y'
         self.flag_col_name = 'user_flag'
         self.flag_map = {'CHECKED': 0, 'BAD': 1, 'UNCHECKED': -1}
         self.flag2markers = dict(zip([-1, 0, 1], ['triangle', 'circle', 'x']))
         self.flag2colors = dict(zip([-1, 0, 1], ['blue', 'green', 'red']))
-        self.do_list = [] # do list builds when new stuff is marked
-        self.redo_list = [] # only builds when undo is executed and reset when new stuff is marked
+        self.do_list = []  # do list builds when new stuff is marked
+        self.redo_list = []  # only builds when undo is executed and reset when new stuff is marked
         self.df = df
         self.df_ref = df_ref
         self.value_col_index = df.columns.get_loc(self.y_col_name)
@@ -40,10 +41,10 @@ class FlagChecker(param.Parameterized):
         selection = streams.Selection1D(source=self.points)
         self.dmap = hv.DynamicMap(self.mark_flag, streams=[selection])
 
-    @param.depends('flag','undo_level')
+    @param.depends('flag', 'undo_level')
     def view(self):
         curves = hv.Curve(self.df_ref) * self.points * self.dmap
-        return curves.opts(width=700, height=400, title='Ready to mark selections: '+self.flag)
+        return curves.opts(width=700, height=400, title='Ready to mark selections: ' + self.flag)
 
     def create_undo_redo_buttons(self):
         backward = pn.widgets.Button(name='\u25c0', width=50)
@@ -55,17 +56,16 @@ class FlagChecker(param.Parameterized):
     def _update_dfg_vals(self):
         self.dfg.iloc[:, self.value_col_index] = self.df_flagged.iloc[:,
                                                                       self.value_col_index]
-        self.dfg.iloc[self.df_flagged[self.flag_col_name] > 0,
-                      self.value_col_index] = np.nan
+        self.dfg.loc[self.df_flagged[self.flag_col_name] > 0, self.y_col_name] = np.nan
 
     def do_mark(self, mark_data):
         self.do_list.append(mark_data)
-        if len(self.redo_list) > 0: # clear out redos if marking again
+        if len(self.redo_list) > 0:  # clear out redos if marking again
             self.redo_list = []
             self.undo_level = 0
 
     def undo_mark(self, event=None):
-        if len(self.do_list) == 0: 
+        if len(self.do_list) == 0:
             return
         mark_data = self.do_list.pop()
         self.redo_list.append(mark_data)
@@ -73,7 +73,7 @@ class FlagChecker(param.Parameterized):
         self.df_flagged.loc[df_flag_selected.index, [
             self.flag_col_name]] = df_flag_selected[self.flag_col_name]
         self._update_dfg_vals()
-        self.undo_level+=1
+        self.undo_level += 1
 
     def redo_mark(self, event=None):
         if len(self.redo_list) == 0:
